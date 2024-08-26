3ad1890163ddcf38cf109991917a17d557c5f5e3
yeunsuh <yeunsuh.kim@gmail.com>
2024-08-27 00:36:49 +0900

    FIX : dummy api removed qna answer api and merged it to qna api

--- src/api/dummy.js
+++ src/api/dummy.js
@@ -1,30 +1,22 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import { QnaDocuments, HoneyDocuments,QnaAnswers } from '../schemas/docs.js';
+import { User } from '../schemas/user.js';
 
 const router = express.Router();
 
 router.get('/dummy/qna', async (req, res) => {
-    QnaDocuments.findOne()
-    .then((result)=>{
+    try{
+        const result = await QnaDocuments.findOne();
+        const answerData = await QnaAnswers.findById(result.answer_list[0].Ranswer,{_id:0,Rqna:0});
+        const answerUserData = await User.findById(result.answer_list[0].Ruser, {level:1, hakbu:1, name:1});
+        result.answer_list=[{...answerData._doc, ...answerUserData._doc, user_grade:result.answer_list[0].user_grade}];
         res.status(200).json(result);
-        console.log(result);
-    })
-    .catch((e)=>{
+    }
+    catch(e){
         console.error(e);
         res.status(500).send('Internal Server Error');
-    });
-});
-
-router.get('/dummy/qnaAnswer', async (req, res) => {
-    QnaAnswers.findOne()
-    .then((result)=>{
-        res.status(200).json(result);
-    })
-    .catch((e)=>{
-        console.error(e);
-        res.status(500).send('Internal Server Error');
-    });
+    }
 });
 
 router.get('/dummy/tips', async (req, res) => {
