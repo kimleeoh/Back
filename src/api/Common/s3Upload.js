2d2b5688e6c40ab4028b5b6b95498feea81b4da2
kim nan seul <154604350+seulnan@users.noreply.github.com>
2024-09-01 21:49:34 +0900

    ADD: 파일구조변경+commonapi

---
+++ src/api/Common/s3Upload.js
@@ -0,0 +1,24 @@
+const AWS = require("aws-sdk");
+const multer = require("multer");
+const multerS3 = require("multer-s3");
+
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_SECRET_KEY,
+  region: process.env.AWS_REGION,
+});
+
+const s3 = new AWS.S3();
+
+const s3upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: process.env.S3_BUCKET,
+    acl: "public-read",
+    key: function (req, file, cb) {
+      cb(null, `uploads/${Date.now()}_${file.originalname}`);
+    },
+  }),
+});
+
+module.exports = upload;
