01d642d49b834bb8f586d9fa790e6473d7d1e33d
kim nan seul <154604350+seulnan@users.noreply.github.com>
2024-09-12 19:58:28 +0900

    MODIFY: 미완성라우터 삭제

--- src/routes/clientRoutes.js
+++ src/routes/clientRoutes.js
@@ -23,21 +23,11 @@ router.delete("/logout", handleLogout);
 router.post("/login/key", handleKeyRequest);
 
 // 회원가입 관련 라우터
-router.post("/register/page/:page", handleRegister);
-router.post("/register/email", handleEmail);
-router.post("/register/emailAuthNum", handleEmailAuthNum);
 
 // qna 관련 라우터
-router.get("/qna/{questionID}", handleRegister);
-router.get("/qna", handleEmail);
-router.post("/qna/create/post", handleEmailAuthNum);
-router.post("/qna/update", handleEmailAuthNum);
-router.post("/qna/create/answer", handleEmailAuthNum);
-router.post("/qna/modify", handleEmailAuthNum);
-router.put("/qna/manage", handleEmailAuthNum);
 
 // tips 관련 라우터
-router.post("/treding/modify", handleEmailAuthNum);
+
 // user 관련 라우터
 
 // common 관련 라우터
