fc5d8b3f93d3d937c9a40f7d8af3f15aa12216ee
kim nan seul <154604350+seulnan@users.noreply.github.com>
2024-10-09 20:30:36 +0900

    [FIX] redishandler로 잘못복붙한거 수정

--- src/config/s3Handler.js
+++ src/config/s3Handler.js
@@ -108,24 +108,82 @@
 
 // export default s3Handler;
 
+import { S3 } from "@aws-sdk/client-s3";
+import { Upload } from "@aws-sdk/lib-storage";
 
-import redis from "redis";
+const s3Handler = (() => {
+    let currentFileNums = {
+        profile: 0,
+        preview: 0,
+        files: 0,
+        Q: 0,
+        A: 0,
+        confirm: 0,
+        badge: 0,
+    };
 
-const redisHandler = (() => {
-    let redisClient = null;
+    let S3client = 0;
+    let bucketName = "nah";
 
     return {
-        create: (redisUrl) => {
-            redisClient = redis.createClient({ url: redisUrl });
+        create: (envWrap) => {
+            S3client = new S3({
+                region: envWrap[0],
+                credentials: {
+                    accessKeyId: envWrap[1],
+                    secretAccessKey: envWrap[2],
+                },
+            });
+            bucketName = envWrap[3];
         },
-        connect: () => {
-            redisClient
-                .connect()
-                .then(() => console.log("Successfully connected to redis"))
+        connect: async () => {
+            await S3client.getObject({ Bucket: bucketName, Key: "test.png" })
+                .then((result) => {
+                    if (result != undefined) {
+                        console.log("Successfully connected to S3");
+                    }
+                })
                 .catch((e) => console.error(e));
         },
-        getRedisClient: () => redisClient,
+        get: async (imgLink) => {
+            S3client.getObject({ Bucket: bucketName, Key: imgLink }).then(
+                (result) => {
+                    return result.Body;
+                }
+            );
+        },
+        put: async (fileDestination, img) => {
+            const u = new Upload({
+                client: S3client,
+                params: {
+                    Bucket: bucketName,
+                    Key:
+                        fileDestination +
+                        "/" +
+                        currentFileNums[fileDestination] +
+                        ".jpg",
+                    Body: img,
+                },
+            });
+
+            await u.done();
+            // await S3client.putObject({
+            //     Bucket: bucketName,
+            //     Key:
+            //         fileDestination +
+            //         "/" +
+            //         currentFileNums[fileDestination] +
+            //         ".jpg",
+            //     Body: img,
+            // });
+            const link = `https://d1bp3kp7g4awpu.cloudfront.net/confirm/${currentFileNums[fileDestination]}.jpg`;
+            currentFileNums[fileDestination]++;
+            return link;
+        },
+        delete: async (imgLink) => {
+            await S3client.deleteObject({ Bucket: bucketName, Key: imgLink });
+        },
     };
 })();
 
-export default redisHandler;
+export default s3Handler;
