2eb81bbe141d6ba7642c256f37aefd5cbea9e9c3
yeunsuh <yeunsuh.kim@gmail.com>
2024-10-15 02:10:08 +0900

    ???

--- src/api/Board/Common/notify.js
+++ src/api/Board/Common/notify.js
@@ -7,7 +7,7 @@ const handleNotify = async (req, res) => {
         const redisClient = redisHandler.getRedisClient();
         mainInquiry.inputRedisClient(redisClient);
     }
-    const r = await mainInquiry.read(['Rnotify_list', 'notify_type_list'], req.decryptedSessionId);
+    const r = await mainInquiry.read(['Rnotify_list'], req.decryptedSessionId);
     const notifyList = await Notify.find({ _id: { $in: r.Rnotify_list } }).lean();
     res.status(200).json({ notifyList });}
     catch(e){
