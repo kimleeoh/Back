6c50698b1d629d4fd1a11e3afaeab8c700879b5a
yeunsuh <yeunsuh.kim@gmail.com>
2024-11-08 13:41:19 +0900

    prob fix

--- src/admin/adminAuthcodeSafer.js
+++ src/admin/adminAuthcodeSafer.js
@@ -14,9 +14,11 @@ const ADMIN_AUTH_CODE = (() => {
         console.log(`Auth code updated to: ${authCode}`);
         axios.get('http://localhost:4502/admin/session-time-left')
     .then((response) => {
-        if (response.status === 200) {
+        if (response.status === 200 && response.data.time>0) {
             response.data.session.user.authCode = authCode;
             alert(`남은 로그인 시간: ${response.data.time}\n시간 초과 시 재로그인하세요.`);
+        }else{
+            alert('세션이 만료되었습니다. 다시 로그인하세요.');
         }
     })
     .catch((error) => {
