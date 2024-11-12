c8228f2fd380b0a5468a5a05434e73dd7ee66d78
yeunsuh <yeunsuh.kim@gmail.com>
2024-11-13 08:31:24 +0900

    [FIX] s3

--- src/index.js
+++ src/index.js
@@ -70,6 +70,7 @@ let store;
             AWS_SECRET_ACCESS_KEY,
             AWS_S3_BUCKET,
         ]);
+        s3Handler.connect(redisClient);
         console.log("S3 configuration completed");
 
         // MongoDB 연결
