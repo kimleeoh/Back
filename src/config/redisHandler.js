064eba3cafd21f49bd7a90c35ef24e62de9cf65f
kim nan seul <154604350+seulnan@users.noreply.github.com>
2024-11-13 08:45:02 +0900

    Reapply "[BUG] 관리자서버"
    This reverts commit b9e71a2a1452ac87af48e403b1ecc54f82b75981.

--- src/config/redisHandler.js
+++ src/config/redisHandler.js
@@ -4,8 +4,15 @@ const redisHandler = (() => {
     let redisClient = null;
 
     return {
-        create: (redisUrl) => {
-            redisClient = redis.createClient({ url: redisUrl });
+        create: async (redisUrl) => {
+            try {
+                redisClient = redis.createClient({ url: redisUrl });
+                await redisClient.connect();
+                console.log("Successfully connected to Redis in create/redishandler");
+            } catch (error) {
+                console.error("Failed to connect to Redis:", error);
+                redisClient = null; // 연결 실패 시 클라이언트 초기화
+            }
         },
         // Redis에 데이터를 저장하는 set 함수
         set: async (key, value) => {
@@ -20,13 +27,13 @@ const redisHandler = (() => {
                 throw error;
             }
         },
-        connect: () => {
-            redisClient
-                .connect()
-                .then(() => console.log("Successfully connected to redis"))
-                .catch((e) => console.error(e));
+        getRedisClient: () => {
+            if (!redisClient) {
+                console.error("Redis client is not initialized.");
+                throw new Error("Redis 클라이언트가 초기화되지 않았습니다.");
+            }
+            return redisClient;
         },
-        getRedisClient: () => redisClient,
     };
 })();
 
