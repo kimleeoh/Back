b54893227e8edc9090b33cb7b3737160734dabb5
kim nan seul <154604350+seulnan@users.noreply.github.com>
2024-11-15 07:08:33 +0900

    Update rateLimiter.js

--- src/functions/rateLimiter.js
+++ src/functions/rateLimiter.js
@@ -22,7 +22,7 @@ const limiter =(()=>{
         }),
         lightRate : ()=> rateLimit({
             windowMs: 1* 60 * 1000, // 1 minute
-            max: 200, // limit each IP to 5 requests per windowMs
+            max: 500, // limit each IP to 5 requests per windowMs
             message: "Too many requests, please try again later."
         }),
     }
