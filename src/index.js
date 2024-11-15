10ca609c127b503b82fcd555b543d4dd7d0d5393
yeunsuh <yeunsuh.kim@gmail.com>
2024-11-15 22:17:11 +0900

    temporary

--- src/index.js
+++ src/index.js
@@ -136,7 +136,7 @@ adminApp.use("/", adminRoutes);
 clientApp.use("/api/lg", limiter.loginRate(), loginRouter);
 clientApp.use("/api", limiter.lightRate(), lightRouter);
 clientApp.use("/api/h", limiter.heavyRate(), heavyRouter);
-clientApp.use("/api/c", limiter.categoryRate(), categoryRouter);
+clientApp.use("/api/c", categoryRouter);
 clientApp.get("/", (req, res) => {
     res.send("<h1>서버 실행 중</h1>");
 });
